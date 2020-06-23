e3a813b6818c9362e9c200d04c0bf049fd8274c1
Jacques Maarek <jacques.maarek@hetic.net>
2020-06-23 17:26:37 +0200

    Feat (backend): updated routes and env file.

---
+++ routes/Router.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"github.com/JackMaarek/spiderMail/controllers"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func SetupRouter(router *gin.Engine){
+	router.POST("/login", controllers.Login)
+	router.POST("/registration", controllers.Registration)
+	router.GET("/users", controllers.GetUsers)
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
+	})
+}
